fad9c2985f91dde474ac030c71430c17f42fcc1e
NissrineELMNIAI <nissrineelmniai0@gmail.com>
2025-11-08 07:18:25 +0100

    nissrine

--- app/src/main/java/com/example/rhapp/ProfileActivity.java
+++ app/src/main/java/com/example/rhapp/ProfileActivity.java
@@ -29,6 +29,15 @@ public class ProfileActivity extends AppCompatActivity {
         LinearLayout conges_interface = findViewById(R.id.conges_interafce);
         @SuppressLint({"MissingInflatedId", "LocalSuppress"})
         LinearLayout employes_interface = findViewById(R.id.employes_interface);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        LinearLayout modifier_profil = findViewById(R.id.modifier_profil);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        LinearLayout security_interface = findViewById(R.id.security_interface);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        LinearLayout help_support = findViewById(R.id.help_support);
+        @SuppressLint("WrongViewCast")
+        Button sedeconnecter = findViewById(R.id.sedeconnecter);
+
 
 
         reunions_interface.setOnClickListener(new View.OnClickListener(){
@@ -52,6 +61,35 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        modifier_profil.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+        security_interface.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(ProfileActivity.this, SecurityActivity.class);
+                startActivity(intent);
+            }
+        });
+        help_support.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(ProfileActivity.this, HelpSupportActivity.class);
+                startActivity(intent);
+            }
+        });
+         sedeconnecter.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
