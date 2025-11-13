2eebe7e2419ef78099737e57d634fa2368d2b6ac
Lamyae HAMDAOUI <lamyae.hamdaoui.23@ump.ac.ma>
2025-11-13 11:07:51 +0100

    lamyae

--- app/src/main/java/com/example/rhapp/reunionActivity.java
+++ app/src/main/java/com/example/rhapp/reunionActivity.java
@@ -1,6 +1,11 @@
 package com.example.rhapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +20,68 @@ public class reunionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_reunion);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // ******************************* Passer d'une fenetre a l'autre ************************************
+
+        LinearLayout accueil = findViewById(R.id.accueil);
+        LinearLayout employes = findViewById(R.id.employes);
+        LinearLayout conge = findViewById(R.id.conge);
+        LinearLayout profil = findViewById(R.id.profil);
+
+        accueil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(reunionActivity.this, AcceuilRhActivity.class));
+
+            }
         });
+        employes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(reunionActivity.this, employeActivity.class));
+
+            }
+        });
+        conge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(reunionActivity.this, CongesActivity.class));
+
+            }
+        });
+        profil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(reunionActivity.this, ProfileActivity.class));
+
+            }
+        });
+
+        // ********************************** Planifier un reunion ********************
+
+        Button btnPlanifierReunion = findViewById(R.id.btnPlanifierReunion);
+
+        btnPlanifierReunion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(reunionActivity.this, PlanifierReunionActivity.class));
+            }
+        });
+
+        // ********************************** Notifications ********************
+
+        RelativeLayout notifications = findViewById(R.id.notifications);
+
+        notifications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(reunionActivity.this, NotificationsRhActivity.class));
+            }
+        });
+
+        // ***************************************************************
+
+
+
     }
 }
