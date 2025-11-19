e43b58c62b0d3b1656d7b939d36418d6a491342f
ikramchahlal <ikram.chahlal.23@ump.ac.ma>
2025-11-19 14:48:34 +0100

    Add files via upload

--- app/src/main/java/com/example/rhapp/AcceuilRhActivity.java
+++ app/src/main/java/com/example/rhapp/AcceuilRhActivity.java
@@ -1,6 +1,11 @@
 package com.example.rhapp;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +20,41 @@ public class AcceuilRhActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_acceuil_rh);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        LinearLayout reunions_interface = findViewById(R.id.reunions_interface);
+        LinearLayout conges_interface = findViewById(R.id.conges_interafce);
+        LinearLayout employes_interface = findViewById(R.id.employes_interface);
+         LinearLayout profile_interface = findViewById(R.id.profile_interface);
+
+
+        reunions_interface.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(AcceuilRhActivity.this, reunionActivity.class);
+                startActivity(intent);
+            }
         });
+        conges_interface.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(AcceuilRhActivity.this, CongesActivity.class);
+                startActivity(intent);
+            }
+        });
+        employes_interface.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(AcceuilRhActivity.this, EmployeActivity.class);
+                startActivity(intent);
+            }
+        });
+        profile_interface.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(AcceuilRhActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
