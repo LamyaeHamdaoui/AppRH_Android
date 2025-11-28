8edebc04fef56752d6cc9bdcde0a8aacfc2aa31b
NissrineELMNIAI <nissrineelmniai0@gmail.com>
2025-11-28 14:53:51 +0100

    Merge remote-tracking branch 'origin/main'

--- app/src/main/java/com/example/rhapp/AcceuilEmployeActivity.java
+++ app/src/main/java/com/example/rhapp/AcceuilEmployeActivity.java
@@ -29,7 +29,7 @@ public class AcceuilEmployeActivity extends AppCompatActivity {
             if (congesfooter != null) {
                 congesfooter.setOnClickListener(v -> {
                     // Remplacer ProfileActivity.class par la classe réelle
-                    startActivity(new Intent(AcceuilEmployeActivity.this, CongesEmploye.class));
+                    startActivity(new Intent(AcceuilEmployeActivity.this, CongesEmployeActivity.class));
                 });
                 if (reunionsfooter != null) {
                     reunionsfooter.setOnClickListener(v -> {
